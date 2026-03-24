174f436082000f10824734c2a76f41d1c3d15c28
SalmanAli <ssalmanaali80@gmail.com>
2026-03-24 16:15:37 +0500

    Test File Added + Zod Validation

--- backend/validation/zod.js
+++ backend/validation/zod.js
@@ -21,3 +21,27 @@ export const loginSchema=z.object({
     password:z.string().min(8).max(20),
 
 })
+
+
+export const creatorProfileSchema = z.object({
+    bio: z.string().min(10).max(80),
+    pageName: z.string().min(3).max(20),
+    pageUrl: z.string().min(3).max(30),
+    profileImageUrl: z.string().url().optional(),
+    bannerUrl: z.string().url().optional(),
+    socialLinks: z.array(z.string().url()).optional(),
+    aboutPage: z.string().max(200).optional(),
+})
+
+export const subscriptionTierSchema = z.object({
+    tierName: z.enum(["regular", "premium"]),
+    price: z.number().positive(),
+    description: z.string().optional(),
+    perks: z.array(z.string()).optional(),
+    
+})
+
+export const subscriptionSchema = z.object({
+    creatorId: z.string().min(1),
+    tierType: z.enum(["regular", "premium"]),
+})
