d5838490c50bfd1562e7b6e76021b82ca4913f53
Haider Ali <hyder6291@gmail.com>
2026-04-01 18:51:40 +0500

    Added Delete Collection Controller

--- backend/controllers/collection.controller.js
+++ backend/controllers/collection.controller.js
@@ -98,11 +98,22 @@ export async function updateCollection(req,res){
   
     let update={}
     if(data.title){
-        update.title=data.title
+       
+     const alreadyTitle=await Collection.findOne({title:data.title,creatorId:req.user.userId});
+     if(alreadyTitle){
+        return res.status(400).json({
+            success:false,
+            error:"The Collection with Same title Already Exists."
+        })
+     }
+     else{
+         update.title=data.title
+     }
     }
     if(data.description){
         update.description=data.description
     }
+    
      const updatedCollection= await Collection.findByIdAndUpdate(collectionId,{
         title:data.title,
         creatorId:req.user.userId,
@@ -123,4 +134,44 @@ export async function updateCollection(req,res){
        }) 
     }
 }
+export async function deleteCollection(){
 
+    try {
+      const {collectionId}=req.params;
+          if(!mongoose.Types.ObjectId.isValid(collectionId)){
+         return res.status(400).json({
+            success:false,
+            error:"Invalid Collection ID"
+         })   
+     }
+     const already=await Collection.findById(collectionId)
+     if(!already){
+        return res.status(400).json({
+            success:false,
+            error:"The Collection Doesnot Exists"
+        })
+     }
+
+      if(req.user.userId.toString()!==already.creatorId.toString()){
+        return res.status(403).json({
+            success:false,
+            message:"Unauthorized Access. Cannot Update"
+        })
+     }
+
+        await Collection.findByIdAndUpdate(collectionId);
+
+        return res.status(200).json({
+            success:true,
+            message:"Collection Deleted Successfully"
+        })
+
+    } catch (error) {
+       console.log("Error in Collection Deletion : ",error);
+       return res.status(500).json({
+        success:false,
+        error:"Internal Server Error"
+       }) 
+    }
+
+}
