bdf4c17de24225954b25b14c10c164b3f38e8b1a
Haider Ali <hyder6291@gmail.com>
2026-04-28 23:02:36 +0500

    Added Bare Minimm Rate Limiter

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,16 @@ import { setupSocketIO } from "./socket/index.js";
 import exploreRouter from "./routes/explore.routes.js"
 import insightsRouter from "./routes/insights.routes.js"
 import contentAccessRouter from "./routes/contentAccess.routes.js";
+import { rateLimit } from 'express-rate-limit'
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+})
 
 
 const app = express();
@@ -37,6 +47,7 @@ app.use('/api/webhooks', webhookRoutes);
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(limiter)
 
 // Routes
 app.get("/", (req, res) => res.send("CONTREON API"));
